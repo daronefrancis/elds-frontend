a99c69cb20a9b028417e4c237caa54da0d7350fd
Darone Francis <daronefrancis@mac.home>
2025-04-05 23:30:22 -0400

    Initial commit for frontend

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// frontend/src/App.jsx
+
+import React from 'react';
+import DashboardView from './components/DashboardView';
+
+function App() {
+  return (
+    <main className="min-h-screen bg-white text-gray-900 p-6">
+      <DashboardView />
+    </main>
+  );
+}
+
+export default App;
